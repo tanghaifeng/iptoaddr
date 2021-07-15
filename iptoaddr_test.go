69883b96215ae805e61438ea30755756cccba23d
TIM <tanghaifeng93@gmail.com>
2021-07-15 17:46:41 +0800

    修改

--- iptoaddr_test.go
+++ iptoaddr_test.go
@@ -7,10 +7,10 @@ import (
 
 /**
   连续只能查询25次
- */
+*/
 func TestGet(t *testing.T) {
-	for i:=0;i <26;i++ {
-		resp := IpToAddr("1.41.102.1")
-		t.Log(resp +"------------------"+strconv.Itoa(i))
+	for i := 0; i < 26; i++ {
+		resp, _ := IpToAddr("1.41.102.1")
+		t.Log(resp + "------------------" + strconv.Itoa(i))
 	}
 }
--- iptool.go
+++ iptool.go
@@ -7,14 +7,16 @@ import (
 )
 
 // 根据IP查询具体的地址 使用的是ip.tool.chinaz.com 查看的结果
-func IpToAddr(ip string) string {
+func IpToAddr(ip string) (string, error) {
 	resp, err := http.Get("http://ip.tool.chinaz.com/" + ip)
+	defer resp.Body.Close()
 	if err != nil {
 		fmt.Println("查询出现错误")
+		return "", err
 	}
-	defer resp.Body.Close()
-	doc, err := goquery.NewDocumentFromReader(resp.Body)
-	addr := doc.Find(".Whwtdhalf em")
-	fmt.Println(addr.Text())
-	return addr.Text()
+	if doc, err := goquery.NewDocumentFromReader(resp.Body); err == nil {
+		addr := doc.Find(".Whwtdhalf em")
+		return addr.Text(), err
+	}
+	return "", err
 }
